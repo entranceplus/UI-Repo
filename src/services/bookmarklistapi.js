ad25138b88abf8e109d1262376c0c96b67764913
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2017-11-17 20:08:04 +0530

    use axios as http client

--- src/services/bookmarklistapi.js
+++ src/services/bookmarklistapi.js
@@ -1,32 +1,21 @@
+var axios = require('axios');
+
+var client = axios.create({
+  baseURL: "https://links.entranceplus.in",
+  timeout: 1000,
+  headers: {'Authorization': 'bearer ' + localStorage.getItem('access_token')}
+});
+
 export function bookmarkapi(url, tags, actionCallback) {
-  let postUrl = 'https://links.entranceplus.in/links';
   let postData = { 'url': url, 'tags': tags };
 
-  fetch(postUrl, {
-    credentials: 'same-origin',
-    method: 'POST',
-    mode: 'cors',
-    headers: new Headers({
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Authorization': 'bearer '+localStorage.getItem('access_token')
-    }),
-    body: JSON.stringify(postData)
-  }).then(response => response.json())
-    .then(json => actionCallback(json));
+  client.post('/links', postData)
+    .then(response => response.data)
+    .then(json => actionCallback(json))
+    .catch(error => console.log(error));
 }
 
 export function bookmarkListapi() {
-  let postUrl = 'https://links.entranceplus.in/links';
-
-  fetch(postUrl, {
-    credentials: 'same-origin',
-    method: 'GET',
-    mode: 'cors',
-    headers: new Headers({
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Authorization': 'bearer '+localStorage.getItem('access_token')
-    })
-  }).then(response => response.json())
+  client.get("/links")
+    .then(response => response.data)
 }
